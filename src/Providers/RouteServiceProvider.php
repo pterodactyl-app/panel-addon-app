dce746d7632eda64eafa013d1d1b4c10db407e78
root <root@vm0.pterodactyl.yvanwatchman.nl>
2019-05-29 14:25:09 +0000

    Base commit

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace YWatchman\Panel_Console\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to the controller routes in your routes file.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'YWatchman\Controllers';
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map()
+    {
+        Route::middleware(['api'])->prefix('/api/app/admin')
+            ->namespace($this->namespace . '\Admin\Api')
+            ->group(__DIR__ . 'Routes/api-app-admin.php');
+
+        Route::middleware(['client-api'])->prefix('/api/app/client')
+            ->namespace($this->namespace . '\Api\User')
+            ->group(__DIR__ . 'Routes/api-app-user.php');
+    }
+}
